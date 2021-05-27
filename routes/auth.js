67fa50c3eb9d09d5388950e0b4d166657ab2c423
Adam <adamafriansyahb@gmail.com>
2021-05-27 13:29:13 +0700

    Finish register route

--- routes/auth.js
+++ routes/auth.js
@@ -12,15 +12,21 @@ const secret = "secre3th3hE";
 router.post('/register', async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 8);
-        const user = new User({
+
+        const user = await User.findOne({email: req.body.email});
+        if (user) {
+            return res.status(500).send("User already exists.");
+        }
+
+        const newUser = new User({
             name: req.body.name,
             email: req.body.email,
             password: hashedPassword
         });
 
-        await user.save();
+        await newUser.save();
 
-        const token = jwt.sign({id: user._id}, secret, {
+        const token = jwt.sign({id: newUser._id}, secret, {
             expiresIn: 86400
         });
 
@@ -38,12 +44,20 @@ router.get('/me', (req, res) => {
         return res.status(401).send({auth: false, message: "No token provided."});
     }
 
-    jwt.verify(token, secret, (err, decoded) => {
+    jwt.verify(token, secret, async (err, decoded) => {
         if (err) {
             return res.status(500).send({auth: false, message: "Failed to authenticate."});
         }
 
-        res.status(200).send(decoded);
+        try {
+            const user = await User.findById(decoded.id, {password: 0});
+            if (!user) return res.status(404).send("User not found.");
+
+            res.status(200).send(user);
+        }
+        catch(err) {
+            res.status(500).send(err.message);
+        }
     });
 });
 
